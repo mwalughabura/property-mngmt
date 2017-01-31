687b04569930f1ee7226d88caf3ec0ba3cab0f7e
mwalughabura <mwalughabura@gmail.com>
2017-01-31 14:11:02 +0300

    managed to create a custom post an empty custom post type

--- index.php
+++ index.php
@@ -1,25 +1,102 @@
 <?php
-   /*
-   Plugin Name: Property Management
-   Plugin URI: http://mwalughabura.wordpress.com
-   Description: A plugin that allows one to manage property.
-   Version: 0.0.1
-   Author: Mwalugha Bura
-   Author URI: http://mrtotallyawesome.com
-   License: GPL2
-   */
+/*
+*Plugin name: Property Management
+*Plugin URI: www.github.com/mwalugha
+*Description: You want to manage your property easily!! You're at the right place.
+*Author: Mwalugha Bura
+*Author URI: www.github.com/mwalughabura
+*Version: 1.0
+*License: GPLv2
+*/
 
-
-function property_add_view() {
-   if(is_single()) {
-      global $post;
-      $current_views = get_post_meta($post->ID, "property_views", true);
-      if(!isset($current_views) OR empty($current_views) OR !is_numeric($current_views) ) {
-         $current_views = 0;
-      }
-      $new_views = $current_views + 1;
-      update_post_meta($post->ID, "property_views", $new_views);
-      return $new_views;
-   }
+if (!defined('ABSPATH')) {
+	exit;
 }
-?>
+
+function wpsc_register_post_type()
+{
+	$singular = 'Property Listing';
+	$plural = 'Property Listings';
+
+	$labels  = array(
+		'name'				=> $plural, 
+		'singular_name' 	=> $singular,
+		'add_name' 			=> 'Add New',
+		'add_a_new_item'	=> 'Add New' . $singular,
+		'edit' 				=> 'Edit',
+		'edit_item' 		=> 'Edit' . $singular,
+		'new_item' 			=> 'New' . $singular,
+		'view' 				=> 'View' . $singular,
+		'view_item' 		=> 'View' . $singular,
+		'search_term' 		=> 'Search' . $plural,
+		'parent'			=> 'Parent' . $singular,
+		'not_found'			=> 'No ' . $plural . 'found',
+		'not_found_in_trash'=> 'No ' . $plural . 'in Trash'
+	);
+
+	$args = array(
+		'labels'				=> $labels,
+		'public' 				=> true,
+		'publicly_quriable' 	=> true,
+		'exclude_from_search'	=> false,
+		'show_in_nav_menus'		=> true,
+		'show_ui'				=> true,
+		'show_in_menu'			=> true,
+		'show_in_admin_bar'		=> true,
+		'menu_position'			=> 6,
+		'menu_icon'				=> 'dashicons_businessman',
+		'can_export'			=> true,
+		'delete_with_user'		=> false,
+		'heirarchical'			=> false,
+		'has_archive'			=> true,
+		'query_var'				=> true,
+		'capability_type'		=> 'post',
+		'map_meta_cap'			=> true,
+		'rewrite'				=> array(
+			'slug' => 'properties',
+			'with_front' => true,
+			'pages'	=> true,
+			'feeds' => true,
+		),
+		'supports'				=> array(
+			'title',
+			'author',
+			'custom_fields'
+		),
+	);
+
+	register_post_type('property', $args);
+}
+
+add_action('init', 'wpsc_register_post_type');
+
+
+add_filter( 'rwmb_meta_boxes', 'your_prefix_meta_boxes' );
+function your_prefix_meta_boxes( $meta_boxes ) {
+    $meta_boxes[] = array(
+        'title'      => __( 'Test Meta Box', 'textdomain' ),
+        'post_types' => 'property',
+        'fields'     => array(
+            array(
+                'id'   => 'name',
+                'name' => __( 'Property name', 'textdomain' ),
+                'type' => 'text',
+            ),
+            array(
+                'id'   => 'address',
+                'name' => __( 'Address', 'textdomain' ),
+                'type' => 'text',
+            ),
+            array(
+                'id'   => 'status',
+                'name' => __( 'Status', 'textdomain' ),
+                'type' => 'select',
+                'options' => array(
+                	'Developed',
+                	'Undeveloped'
+                )
+            ),
+        ),
+    );
+    return $meta_boxes;
+}
